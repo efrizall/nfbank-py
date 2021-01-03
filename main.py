9159c2a67e70e27647219509d0101595850743f3
efrizall <rizal.ef340@gmail.com>
2021-01-03 09:04:10 +0700

    initial commit

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ def transfer():
     pass
 
 def withdraw():
-    pass
+    rek = input("Masukkan no rekening anda : ")
+    cash = input("Masukkan jumlah uang : ")
 
 def deposit():
-    pass
+    rek = input("Masukkan no rekening anda : ")
+    cash = input("Masukkan jumlah uang : ")
 
 def new_rek():
-    pass
+    name = input("Masukkan nama anda : ")
+    cash = input("Masukkan rekening awal anda : ")
 
 def tanya():
     print("[1] Buka Rekening")
@@ -22,6 +25,8 @@ def tanya():
 
     if pilih == 1:
         print("Buka rekening")
+        new_rek()
+        tanya()
     elif pilih == 2:
         print("Setoran tunai")
     elif pilih == 3:
@@ -31,10 +36,15 @@ def tanya():
     elif pilih == 5:
         print("Lihat daftar")
     elif (pilih == 6):
-        print("Yakin ingin keluar?")
+        ans = input("Yakin ingin keluar? (y/n) : ")
+        if ans == 'y':
+            print("Terima kasih")
+        elif ans == 'n':
+            tanya()
+        else:
+            print("Input anda salah")
+            tanya()
     else:
         print("Masukkan yang anda masukkan salah")
 
 tanya()
-new_rek()
-deposit()
